e3759610619a9b6e8cc0ae63e729e6c17b71af65
Murtaza Sultani <murtaz1392@gmail.com>
2020-09-18 18:58:59 +0430

    Write TailwindCSS Plugin - Add 'dark' variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin')
+const selectorParser = require('postcss-selector-parser');
 
 module.exports = {
     purge: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php', './resources/js/**/*.vue'],
@@ -13,7 +15,23 @@ module.exports = {
 
     variants: {
         opacity: ['responsive', 'hover', 'focus', 'disabled'],
+        backgroundColor: ['responsive', 'dark', 'hover', 'focus'],
+        textColor: ['responsive', 'dark', 'hover', 'focus'],
     },
 
-    plugins: [require('@tailwindcss/ui')],
+    plugins: [
+        require('@tailwindcss/ui'),
+        plugin(function({ addVariant, prefix }) {
+            addVariant('dark', ({ modifySelectors, separator }) => {
+                modifySelectors(({ selector }) => {
+                    return selectorParser((selectors) => {
+                        selectors.walkClasses((sel) =>{
+                            sel.value = `dark${separator}${sel.value}`
+                            sel.parent.insertBefore(sel, selectorParser().astSync(prefix('.theme-dark ')))
+                        })
+                    }).processSync(selector)
+                })
+            })
+        })
+    ],
 };
